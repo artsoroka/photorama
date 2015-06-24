8b6faad015b7ac493e218167f6d7959462dbdc96
artsoroka <githubrepository@yandex.ru>
2015-06-24 16:36:49 +0000

    added vk auth route and api config

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function(req,res){
     res.send('hello photorama'); 
 }); 
 
+app.get(config.routes.VK_LOGIN_REDIRECT, function(req,res){
+    res.send('vk login redirect url '); 
+}); 
+
 app.listen(config.APP_PORT, function(){
     console.log('Photorama is started on a port %d', config.APP_PORT); 
 }); 
--- config/index.js
+++ config/index.js
@@ -1,3 +1,17 @@
+var BASE_URL = 'http://vkstat.ru'; 
+var VK_LOGIN_REDIRECT = '/auth/vk/'; 
+
 module.exports = {
-    APP_PORT: process.env.PHOTORAMA_PORT || 8080
+    APP_PORT: process.env.PHOTORAMA_PORT || 8080, 
+    routes: {
+        VK_LOGIN_REDIRECT: VK_LOGIN_REDIRECT
+    }, 
+    vk: {
+        id: process.env.PHOTORAMA_VK_ID,  
+        secret: process.env.PHOTORAMA_VK_SECRET, 
+        scope: ['photos', 'wall', 'email', 'offline'], 
+        display: 'popup', 
+        api_version: '5.34', 
+        redirect: BASE_URL + VK_LOGIN_REDIRECT 
+    }
 }; 
