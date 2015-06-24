250d40b49d5891c39ff1ef45bf6671b7a80bc7e8
artsoroka <githubrepository@yandex.ru>
2015-06-24 18:02:24 +0000

    social network auth with vk.com

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ var request = require('request');
 var app     = express(); 
 var config  = require(__dirname + '/config'); 
 
+app.use(express.static(__dirname + '/public')); 
+app.set('view engine', 'ejs'); 
+
 app.get('/', function(req,res){
-    res.send('hello photorama'); 
+    
+    var vkLoginData = {
+        client_id    : config.vk.id,  
+        scope        : config.vk.scope, 
+        display      : config.vk.display,  
+        api_version  : config.vk.api_version,  
+        redirect_uri : config.vk.redirect 
+    }; 
+    
+    res.render('mainpage', {
+        login: {
+            vk: JSON.stringify(vkLoginData)
+        }
+    }); 
 }); 
 
 app.get(config.routes.VK_LOGIN_REDIRECT, function(req,res){
-    res.send('vk login redirect url '); 
+    res.render('auth/socialAuth');  
 }); 
 
 app.listen(config.APP_PORT, function(){
---
+++ public/js/auth.js
@@ -0,0 +1,34 @@
+var Auth = {
+    window: null, 
+    provider: null, 
+    vklogin: function(){
+        var url = this.composeUrl('vk'); 
+        this.provider = 'vk'; 
+        this.window = window.open(url, "authentication", "width=800, height=450");
+    },
+    composeUrl: function(provider){
+        if(provider == 'vk'){
+            return this.getVkAuthUrl(); 
+        }
+    }, 
+    getVkAuthUrl:  function(){
+        var credentials = JSON.parse( document.getElementById('vk_login').dataset.credentials ); 
+        var base_url = 'https://oauth.vk.com/authorize?response_type=token'; 
+        var query = []; 
+        for(var property in credentials){
+            if(typeof property == 'object'){
+                ; 
+                query.push( [property, credentials[property].join('')].join('=') ); 
+            } else {
+                query.push([property, credentials[property]].join('=')); 
+            }
+        }
+        
+        return [base_url, query.join('&')].join('&'); 
+         
+    }, 
+    
+    readHash: function(hash){
+        console.log('oauth response: ', hash); 
+    }
+}
